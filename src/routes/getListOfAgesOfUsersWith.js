c213d086db9cba8e6b2d6c6e34e4075d27f26220
Arpita Saha <arpita.sahaa@gmail.com>
2021-12-03 09:28:14 -0500

    Implemented logic in backend

--- src/routes/getListOfAgesOfUsersWith.js
+++ src/routes/getListOfAgesOfUsersWith.js
@@ -5,6 +5,7 @@ const getListOfAgesOfUsersWithHandler = async (request, response) => {
     //Getting the itemToLookup from request path parameter instead of hardcoding
     //Not used body to get the item as usually GET is invoked without a body
     const itemToLookup = request.params.item;
+    // console.log("itemToLookup " + itemToLookup);
     const data = await mockDBCalls.getListOfAgesOfUsersWith(itemToLookup);
     return response.status(200).send(JSON.stringify(data));
 };
