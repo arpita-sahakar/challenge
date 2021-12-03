e3a8fccc1589cf71b05a3564a0264183a45a90c5
Arpita Saha <arpita.sahaa@gmail.com>
2021-12-02 23:04:54 -0500

    Removed hardcoded value

--- src/routes/getListOfAgesOfUsersWith.js
+++ src/routes/getListOfAgesOfUsersWith.js
@@ -2,11 +2,13 @@
 const mockDBCalls = require('../database/index.js');
 
 const getListOfAgesOfUsersWithHandler = async (request, response) => {
-    const itemToLookup = 'carrot';
+    //Getting the itemToLookup from request path parameter instead of hardcoding
+    //Not used body to get the item as usually GET is invoked without a body
+    const itemToLookup = request.params.item;
     const data = await mockDBCalls.getListOfAgesOfUsersWith(itemToLookup);
     return response.status(200).send(JSON.stringify(data));
 };
 
 module.exports = (app) => {
-    app.get('/users/age', getListOfAgesOfUsersWithHandler);
+    app.get('/users/age/:item', getListOfAgesOfUsersWithHandler);
 };
