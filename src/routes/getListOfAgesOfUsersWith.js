e118a4de3f3c2439c8c3c4aacd636ad087411d69
Arpita Saha <arpita.sahaa@gmail.com>
2021-12-04 20:01:26 -0500

    Added error handling

--- src/routes/getListOfAgesOfUsersWith.js
+++ src/routes/getListOfAgesOfUsersWith.js
@@ -2,12 +2,16 @@
 const mockDBCalls = require('../database/index.js');
 
 const getListOfAgesOfUsersWithHandler = async (request, response) => {
-    //Getting the itemToLookup from request path parameter instead of hardcoding
-    //Not used body to get the item as usually GET is invoked without a body
-    const itemToLookup = request.params.item;
-    // console.log("itemToLookup " + itemToLookup);
-    const data = await mockDBCalls.getListOfAgesOfUsersWith(itemToLookup);
-    return response.status(200).send(JSON.stringify(data));
+    try {
+        //Getting the itemToLookup from request path parameter instead of hardcoding
+        //Not used body to get the item as usually GET is invoked without a body
+        const itemToLookup = request.params.item;
+        // console.log("itemToLookup " + itemToLookup);
+        const data = await mockDBCalls.getListOfAgesOfUsersWith(itemToLookup);
+        return response.status(200).send(JSON.stringify(data));
+    } catch (error) {
+        return response.status(500).send(error);
+    }
 };
 
 module.exports = (app) => {
